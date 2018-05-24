7d0b165447325eb81f2b0a3a267b45aaee71a378
Herman Zvonimir Došilović <hermanz.dosilovic@microblink.com>
2018-05-24 17:11:35 +0200

    Added new flag to LayouterDemo. --result-only flag that show only layouter results

--- LayouterDemo/Source/Main.cpp
+++ LayouterDemo/Source/Main.cpp
@@ -10,6 +10,7 @@
 #include <string>
 #include <vector>
 #include <limits>
+#include <cmath>
 
 void initializeCmdParser( cmdline::parser & parser )
 {
@@ -51,6 +52,8 @@ void initializeCmdParser( cmdline::parser & parser )
     parser.add< std::string >( "test-case", 't', "run only specific test case", false, "" );
 
     parser.add< std::string >( "dataset", 'd', "dataset path", false, "" );
+
+    parser.add( "result-only", '\0', "output only result" );
 }
 
 layouter::AlignerVariant selectAligner( std::string const & aligner )
@@ -89,7 +92,7 @@ layouter::CompositeSpacerVariant selectSpacer( std::string const & spacer )
     return layouter::spacer::NoneSpacerParameter{};
 }
 
-void print( char const c, std::size_t const n, char const e )
+void print( char const c, std::size_t const n, char const e = '\n' )
 {
     for ( std::size_t i{ 0 }; i < n; ++i )
     {
@@ -127,6 +130,9 @@ int main( int argc, char ** argv )
     float maxSpacerAccuracy{ std::numeric_limits< float >::min() };
     float avgSpacerAccuracy{ 0.0f };
 
+    float alignerAccuracyResult[ 11 ] = { 0.0f };
+    float spacerAccuracyResult[ 11 ] = { 0.0f };
+
     std::size_t realDatasetSize{ 0 };
 
     for ( auto const & inputEntry : dataset )
@@ -142,13 +148,13 @@ int main( int argc, char ** argv )
         layouter::wide_string filtered;
         std::copy_if
         (
-                std::begin( expected ),
-                std::end  ( expected ),
-                std::back_inserter( filtered ),
-                []( layouter::wide_string::value_type const & c )
-                {
-                    return c != static_cast< layouter::wide_string::value_type >( ' ' );
-                }
+            std::begin( expected ),
+            std::end  ( expected ),
+            std::back_inserter( filtered ),
+            []( layouter::wide_string::value_type const & c )
+            {
+                return c != static_cast< layouter::wide_string::value_type >( ' ' );
+            }
         );
 
         layouter::OcrResult const & alignedResult{ layouter::aligner::align( alignerVariant, std::get< 1 > ( inputEntry ) ) };
@@ -158,14 +164,18 @@ int main( int argc, char ** argv )
         auto const & spacedString{ spacedResult.toString() };
         float spacerAccuracy{ layouter::Metric::editDistance( spacedString, expected ) };
 
-        print( '-', 80, '\n' );
-        std::cout << "Test case        : " << std::get< 0 >( inputEntry ) << '\n';
-        std::cout << "Aligner Accuracy : " << alignerAccuracy << '\n';
-        std::cout << "Spacer Accuracy  : " << spacerAccuracy << '\n';
-        print( '-', 80, '\n' );
-        std::cout << spacedString << '\n';
-        print( '-', 80, '\n' );
-        std::cout << '\n' << '\n' << '\n' << '\n';
+
+        if ( !parser.exist( "result-only" ) )
+        {
+            print( '-', 80 );
+            std::cout << "Test case        | " << std::get< 0 >( inputEntry ) << '\n';
+            std::cout << "Aligner Accuracy | " << alignerAccuracy << '\n';
+            std::cout << "Spacer Accuracy  | " << spacerAccuracy << '\n';
+            print( '-', 80 );
+            std::cout << spacedString << '\n';
+            print( '-', 80 );
+            std::cout << '\n' << '\n' << '\n' << '\n';
+        }
 
         minAlignerAccuracy = std::min( minAlignerAccuracy, alignerAccuracy );
         maxAlignerAccuracy = std::max( maxAlignerAccuracy, alignerAccuracy );
@@ -174,22 +184,46 @@ int main( int argc, char ** argv )
         minSpacerAccuracy = std::min( minSpacerAccuracy, spacerAccuracy );
         maxSpacerAccuracy = std::max( maxSpacerAccuracy, spacerAccuracy );
         avgSpacerAccuracy += spacerAccuracy;
+
+        std::uint8_t const alignerRoundAccuracy{ static_cast< std::uint8_t >( std::floor( alignerAccuracy * 10.0f ) ) };
+        std::uint8_t const spacerRoundAccuracy{ static_cast< std::uint8_t >( std::floor( spacerAccuracy * 10.0f ) ) };
+
+        for ( std::uint8_t i{ 0 }; i <= alignerRoundAccuracy; ++i )
+        {
+            alignerAccuracyResult[ i ]++;
+        }
+
+        for ( std::uint8_t i{ 0 }; i <= spacerRoundAccuracy; ++i )
+        {
+            spacerAccuracyResult[ i ]++;
+        }
     }
 
     avgAlignerAccuracy /= realDatasetSize;
     avgSpacerAccuracy  /= realDatasetSize;
 
-    print( '-', 80, '\n' );
+    print( '-', 80 );
     std::cout << "Results" << '\n';
-    print( '-', 80, '\n' );
-    std::cout << "Min aligner accuracy: " << minAlignerAccuracy << '\n' <<
-                 "Max aligner accuracy: " << maxAlignerAccuracy << '\n' <<
-                 "Avg aligner accuracy: " << avgAlignerAccuracy << '\n' <<
-                 "                    : "                       << '\n' <<
-                 "Min spacer accuracy : " << minSpacerAccuracy  << '\n' <<
-                 "Max spacer accuracy : " << maxSpacerAccuracy  << '\n' <<
-                 "Avg spacer accuracy : " << avgSpacerAccuracy  << '\n';
-    print( '-', 80, '\n' );
+    print( '-', 80 );
+    std::cout << "Min aligner accuracy | " << minAlignerAccuracy << '\n' <<
+                 "Max aligner accuracy | " << maxAlignerAccuracy << '\n' <<
+                 "Avg aligner accuracy | " << avgAlignerAccuracy << '\n' <<
+                 "                     | "                       << '\n' <<
+                 "Min spacer accuracy  | " << minSpacerAccuracy  << '\n' <<
+                 "Max spacer accuracy  | " << maxSpacerAccuracy  << '\n' <<
+                 "Avg spacer accuracy  | " << avgSpacerAccuracy  << '\n';
+
+    std::cout << '\n' << "Aligner Results:" << '\n';
+    for ( std::uint8_t i{ static_cast< std::uint8_t >( std::floor( minAlignerAccuracy * 10.0f ) ) } ; i < 11; ++i )
+    {
+        std::printf( "%3d%% >= %.5f\n", i * 10, alignerAccuracyResult[ i ] / realDatasetSize );
+    }
+    std::cout << '\n' << "Spacer Results:" << '\n';
+    for ( std::uint8_t i{ static_cast< std::uint8_t >( std::floor( minSpacerAccuracy * 10.0f ) ) }; i < 11; ++i )
+    {
+        std::printf( "%3d%% >= %.5f\n", i * 10, spacerAccuracyResult[ i ] / realDatasetSize );
+    }
+    print( '-', 80 );
 
     return 0;
 }
